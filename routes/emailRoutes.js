9b4cec6b9ed84975148eb378ef507322d3e77243
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-11-05 09:47:59 +0100

    Add: Email sender

---
+++ routes/emailRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const transporter = require("../config/email-config")
+const generateEmailTemplate = require("../templates/emailTemplate")
+
+const router = express.Router()
+
+const users = [{ id: 1, name: "Aldin", email: "aldinmusik@gmail.com" }]
+
+router.get("/send", async (req, res) => {
+  for (const user of users) {
+    const mailOptions = {
+      from: process.env.SMTP_USER,
+      to: user.email,
+      subject: "Phishing Simulation Text",
+      html: generateEmailTemplate(user.id),
+    }
+
+    try {
+      await transporter.sendMail(mailOptions)
+      console.log(`Email sent to ${user.email}`)
+    } catch {
+      console.error(`Error sending email to ${user.email}:`, error)
+    }
+  }
+
+  res.send("Emails sent!")
+})
