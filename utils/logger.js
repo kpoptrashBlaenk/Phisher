3274001dd68158affb61c6f3aa972ed0c6a6a460
MUSIC Aldin (SNCF RESEAU / EIC LORRAINE CHAMPAGNE ARDENNE / POLE SECURITE) <0104314K@commun.ad.sncf.fr>
2024-11-05 14:49:38 +0100

    Fix: Log, I hope

--- utils/logger.js
+++ utils/logger.js
@@ -1,20 +1,11 @@
-const fs = require("fs")
-const logFilePath = "./tracking.log"
 const pool = require("../config/databse-config")
 
 const trackingLog = async (userId) => {
   const timestamp = new Date().toISOString()
-  const logEntry = `User ID: ${userId} clicked at ${timestamp}\n`
-
-  fs.appendFile(logFilePath, logEntry, (err) => {
-    if (err) {
-      console.error("Error writing to log file:", err)
-      return res.status(500).send("Error logging the action")
-    }
-  })
-
+  const action = "button_click"
   const query =
     "INSERT INTO tracking_log (user_id, action, timestamp) VALUES ($1, $2, $3)"
+
   try {
     await pool.query(query, [userId, action, timestamp])
     console.log(
