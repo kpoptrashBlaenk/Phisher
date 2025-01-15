21d411dadf6f1e88eb9c4915f3be3fbd33f7d5ec
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-01-15 15:42:20 +0100

    Added middleware

--- app.js
+++ app.js
@@ -4,16 +4,31 @@ const trackingRoutes = require("./routes/tracking-routes")
 const interfaceRoutes = require("./routes/interface-routes")
 const authenticationRoutes = require("./routes/authentication-routes")
 const apiRoutes = require("./routes/api/api-routes")
+const cookieParser = require("cookie-parser")
 
 const app = express()
 
 app.use(express.json()) // To use JSON
 app.use(express.static("public")) // To use static files from public
+app.use(cookieParser()) // To handle cookies
 
-app.use("/email", emailRoutes)
-app.use("/track", trackingRoutes)
-app.use("/", interfaceRoutes)
+const isAuthenticated = (req, res, next) => {
+  if (req.path !== "/authentication/sign") {
+    if (req.cookies.email) {
+      return next()
+    }
+
+    return res.redirect("/authentication/sign")
+  }
+
+  next()
+}
+
+app.use("/email", isAuthenticated, emailRoutes)
+app.use("/track", isAuthenticated, trackingRoutes)
+app.use("/", isAuthenticated, interfaceRoutes)
+app.use("/api", isAuthenticated, apiRoutes)
+
 app.use("/authentication", authenticationRoutes)
-app.use("/api", apiRoutes)
 
 module.exports = app
