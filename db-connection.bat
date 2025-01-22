080be3066fcf09afffa4ae5807f11dbef91710a6
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-01-22 11:21:58 +0100

    automisation in db connection for dev

---
+++ db-connection.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Load .env variables
+for /f "usebackq tokens=1,2 delims==" %%a in ("./.env") do (
+    set %%a=%%b
+)
+
+:: Connect to DB
+set PGPASSWORD=%DB_PASSWORD%
+"%PSQL_PATH%" -h %DB_EXTERNAL_HOST% -p %DB_PORT% -U %DB_USERNAME% -d %DB_NAME%
+
+endlocal
