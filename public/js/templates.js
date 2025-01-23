4262f72489db5ed8990ea64f36e33932dc8699b1
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-01-23 11:21:52 +0100

    tempaltes js now functional

--- public/js/templates.js
+++ public/js/templates.js
@@ -1,7 +1,37 @@
 const templateListElement = document.querySelector("#templateList")
 const allTemplates = [{ name: "Template 1" }, { name: "Template 2" }]
 
+const changeSelectedTemplate = (parentDiv) => {
+  const allDivs = document.querySelectorAll("#templateList .list-group-item")
+
+  // Set all divs to unselected
+  allDivs.forEach((div) => {
+    const buttons = div.querySelectorAll("button")
+    const nameButton = buttons[0]
+    const goButton = buttons[1]
+
+    div.classList.remove("active")
+    nameButton.classList.remove("btn-primary")
+    nameButton.classList.add("btn-light")
+    goButton.classList.remove("btn-light")
+    goButton.classList.add("btn-primary")
+  })
+
+  // Set selected div to active
+  const buttons = parentDiv.querySelectorAll("button")
+  const nameButton = buttons[0]
+  const goButton = buttons[1]
+
+  parentDiv.classList.add("active")
+  nameButton.classList.add("btn-primary")
+  nameButton.classList.remove("btn-light")
+  goButton.classList.add("btn-light")
+  goButton.classList.add("btn-primary")
+}
+
 const createTemplateList = () => {
+  let defaultSelected
+
   allTemplates.forEach((template, index) => {
     // Parent Div
     const div = document.createElement("div")
@@ -12,29 +42,29 @@ const createTemplateList = () => {
       "justify-content-between",
       "p-1"
     )
-    if (index === 0) {
-      div.classList.add("active")
-    }
 
     // Name Button
     const nameButton = document.createElement("button")
-    nameButton.classList.add("btn", "w-100", index === 1 ? "btn-light" : "btn-primary")
+    nameButton.classList.add("btn", "w-100")
     nameButton.textContent = template.name
+    nameButton.addEventListener("click", () => changeSelectedTemplate(div))
 
     // Go Button
     const goButton = document.createElement("button")
-    goButton.classList.add(
-      "btn",
-      index === 1 ? "btn-primary" : "btn-light",
-      index === 1 ? "text-white" : "text-black"
-    )
+    goButton.classList.add("btn")
     goButton.textContent = "Preview"
 
     // Append
     div.appendChild(nameButton)
     div.appendChild(goButton)
     templateListElement.appendChild(div)
+
+    if (index === 0) {
+      defaultSelected = div
+    }
   })
+
+  changeSelectedTemplate(defaultSelected)
 }
 
 createTemplateList()
