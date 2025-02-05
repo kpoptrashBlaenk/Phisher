75296a3399ff47631092077f56335255fad8375b
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-02-05 16:33:31 +0100

    commented logger

--- utils/logger.ts
+++ utils/logger.ts
@@ -2,7 +2,9 @@ import pool from "../config/database-config"
 import axios from "axios"
 import { LogContext } from "../types/database"
 
-export const trackingLog = async (context: LogContext) => {
+// Logger that each link uses
+async function trackingLog(context: LogContext) {
+  // Count how many times this person logged
   const response = await axios.get<{ count: number }>(
     `${process.env.HOST || `http://localhost:${process.env.PORT}`}/api/tracking/count/${
       context.userId
@@ -10,14 +12,22 @@ export const trackingLog = async (context: LogContext) => {
   )
   const count = response.data.count + 1
 
+  // Timestamp
   const timestamp = new Date().toISOString()
-  const query =
-    "INSERT INTO tracking_log (user_id, timestamp, page, message, count) VALUES ($1, $2, $3, $4, $5)"
 
   try {
+    // Insert log
+    const query = `
+    INSERT INTO tracking_log (user_id, timestamp, page, message, count)
+    VALUES ($1, $2, $3, $4, $5)
+    `
+
     await pool.query(query, [context.userId, timestamp, context.page, context.message, count])
+
     console.log(`Logged to database: User ID: ${context.userId} clicked at ${timestamp}`)
   } catch (error) {
     console.error("Error logging to database:", error)
   }
 }
+
+export default trackingLog
