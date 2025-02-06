eae1f7e297c30a5c9bba510d305df9664730fe26
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-02-06 14:22:34 +0100

    authentication middleware as single file now

--- app.ts
+++ app.ts
@@ -1,67 +1,17 @@
 import cookieParser from "cookie-parser"
-import express, { NextFunction, Request, Response } from "express"
-import pool from "./config/database-config"
+import express, { Request, Response } from "express"
 import apiRoutes from "./routes/api/api-routes"
 import authenticationRoutes from "./routes/authentication-routes"
 import interfaceRoutes from "./routes/interface-routes"
 import templateRoutes from "./routes/template-routes"
 import trackingRoutes from "./routes/tracking/tracking-routes"
-import { AdminsRow } from "./types/database"
+import isAuthenticated from "./utils/authentication"
 
 const app = express()
 
 app.use(express.json()) // To use JSON
 app.use(express.static("public")) // To use static files from public
 app.use(cookieParser()) // To handle cookies
-
-// Check if user is authenticated then redirect
-const isAuthenticated = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  console.log(req.path)
-  try {
-    // Get admin with the saved cookies
-    const selectAdminByCoookieQuery = `
-    SELECT *
-    FROM admins
-    WHERE admins.cookies = $1
-    `
-
-    const selectAdminByCoookieResult = await pool.query<AdminsRow>(selectAdminByCoookieQuery, [req.cookies.phisher])
-
-    // Check if cookies are equal to admins cookies
-    const hasCookies =
-      req.cookies.phisher && selectAdminByCoookieResult.rows[0]?.cookies
-        ? selectAdminByCoookieResult.rows[0].cookies === req.cookies.phisher
-        : false
-
-    // If not logging page (sign)
-    if (req.path !== "/authentication/sign" && req.path !== "/sign") {
-      // If has cookies, continue
-      if (hasCookies) {
-        return next()
-      }
-
-      // If searching for api (or authentication because of a bug that makes api/auth become auth), continue
-      if (req.path.startsWith("/api") || req.path.startsWith("/authentication") || req.path.startsWith("/users")) {
-        return next()
-      }
-
-      // Default, go to login page
-      return res.redirect("/authentication/sign")
-    }
-
-    // If has cookies
-    if (hasCookies) {
-      return res.redirect("/")
-    }
-
-    // Fallback, go to login page
-    next()
-  } catch (error) {
-    console.error("Authentication error", error)
-    // instead of return because this must return promise and not void
-    next(error)
-  }
-}
 
 // Routes
 app.use("/track", trackingRoutes)
---
+++ utils/authentication.ts
@@ -0,0 +1,53 @@
+import { NextFunction, Request, Response } from "express"
+import pool from "../config/database-config"
+import { AdminsRow } from "../types/database"
+
+// Check if user is authenticated then redirect
+async function isAuthenticated(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    // Get admin with the saved cookies
+    const selectAdminByCoookieQuery = `
+      SELECT *
+      FROM admins
+      WHERE admins.cookies = $1
+      `
+
+    const selectAdminByCoookieResult = await pool.query<AdminsRow>(selectAdminByCoookieQuery, [req.cookies.phisher])
+
+    // Check if cookies are equal to admins cookies
+    const hasCookies =
+      req.cookies.phisher && selectAdminByCoookieResult.rows[0]?.cookies
+        ? selectAdminByCoookieResult.rows[0].cookies === req.cookies.phisher
+        : false
+
+    // If not logging page (sign)
+    if (req.path !== "/authentication/sign" && req.path !== "/sign") {
+      // If has cookies, continue
+      if (hasCookies) {
+        return next()
+      }
+
+      // If searching for api (or authentication because of a bug that makes api/auth become auth), continue
+      if (req.path.startsWith("/api") || req.path.startsWith("/authentication") || req.path.startsWith("/users")) {
+        return next()
+      }
+
+      // Default, go to login page
+      return res.redirect("/authentication/sign")
+    }
+
+    // If has cookies
+    if (hasCookies) {
+      return res.redirect("/")
+    }
+
+    // Fallback, go to login page
+    next()
+  } catch (error) {
+    console.error("Authentication error", error)
+    // instead of return because this must return promise and not void
+    next(error)
+  }
+}
+
+export default isAuthenticated
