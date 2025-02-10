0c93f0e71f784378ea1e8ff42012d25f4ba22a10
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-02-10 15:17:31 +0100

    mend

--- app.ts
+++ app.ts
@@ -1,13 +1,13 @@
 import cookieParser from "cookie-parser"
 import express, { Request, Response } from "express"
-import adminRoutes from "./controller/api/admin/index"
-import authenticationRoutes from "./controller/api/authentication/index"
-import emailRoutes from "./controller/api/email/index"
-import teamRoutes from "./controller/api/team/index"
+import adminApiRoutes from "./controller/api/admin/index"
+import authenticationApiRoutes from "./controller/api/authentication/index"
+import emailApiRoutes from "./controller/api/email/index"
+import teamApiRoutes from "./controller/api/team/index"
 import trackingRoutes from "./controller/api/tracking/index"
-import userRoutes from "./controller/api/user/index"
+import userApiRoutes from "./controller/api/user/index"
+import authenticationRoutes from "./controller/authentication/index"
 import mainRoutes from "./controller/main/index"
-import signRoutes from "./controller/sign/index"
 import templateRoutes from "./controller/template/index"
 import authOnly from "./middleware/auth"
 import guestOnly from "./middleware/guest"
@@ -19,15 +19,15 @@ app.use(express.static("public")) // To use static files from public
 app.use(cookieParser()) // To handle cookies
 
 // Routes
-app.use("/tracking", trackingRoutes)
+app.use("/api/tracking", trackingRoutes)
 
 app.use("/authentication", guestOnly, authenticationRoutes)
-app.use("/sign", guestOnly, signRoutes)
+app.use("/api/authentication", guestOnly, authenticationApiRoutes)
 
-app.use("/admin", authOnly, adminRoutes)
-app.use("/email", authOnly, emailRoutes)
-app.use("/team", authOnly, teamRoutes)
-app.use("/user", authOnly, userRoutes)
+app.use("/api/admin", authOnly, adminApiRoutes)
+app.use("/api/email", authOnly, emailApiRoutes)
+app.use("/api/team", authOnly, teamApiRoutes)
+app.use("/api/user", authOnly, userApiRoutes)
 app.use("/main", authOnly, mainRoutes)
 app.use("/template", authOnly, templateRoutes)
 
