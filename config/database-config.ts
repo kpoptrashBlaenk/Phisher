03562c2565e9dbd665eeae4b7cf4a022b2939da4
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-02-27 16:55:11 +0100

    db fix

--- config/database-config.ts
+++ config/database-config.ts
@@ -7,10 +7,13 @@ dotenv.config()
 // Database Connection
 const pool = new Pool({
   user: process.env.DB_USERNAME,
-  host: process.env.DB_INTERNAL_HOST || process.env.DB_EXTERNAL_HOST,
+  host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: false,
+  },
 })
 
 export default pool
